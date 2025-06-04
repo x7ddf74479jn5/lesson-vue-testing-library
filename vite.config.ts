9eefffb6576d2b28a8bc6f181c7072ade583b9b3
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2025-06-04 15:18:36 +0900

    chore: format

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 /// <reference types="vitest" />
-// @ts-ignore
-import vue from '@vitejs/plugin-vue';
-import { defineConfig } from 'vite';
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [vue()],
   test: {
-    setupFiles: ['./vitest.setup.ts'],
+    setupFiles: ["./vitest.setup.ts"],
     globals: true,
-    environment: 'happy-dom',
+    environment: "happy-dom",
   },
 });
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,4 +1,2 @@
-import '@testing-library/jest-dom';
-import '@testing-library/jest-dom/vitest';
-/// <reference path="types/vitest.d.ts" />
-/// <reference path="jest.d.ts" />
+import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/vitest";
